46af184364b6c4f12ef81356e4a497e513ec4aa7
CONOR RABBITTE - STUDENT <G00365218@gmit.ie>
2019-02-12 17:40:30 +0000

    Angular_Typescript commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
   title = 'my-first-project';
+
+  counter:number = 0;
+
+  onButtonClick(){
+    this.counter++;
+  }
+
+  hide:boolean = true;
+
+  onImageDBClick(){
+    console.log("Image Clicked");
+    if (this.hide == false) {
+        this.hide = true;
+    }
+    else {
+      this.hide = false;
+    }
+  }
+
 }
